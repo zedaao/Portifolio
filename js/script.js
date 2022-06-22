43ad46c58a7d6689f7b35b3f707d596be4491b9f
Seu Nome <leon.ueda2016@gmail.com>
2022-06-22 17:18:33 -0300

    mais javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 import ScrollSuave from "./modules/scroll-suave.js";
+import ScrollAnima from "./modules/scroll.anima.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+
+const scrollAnima = new ScrollAnima("[data-anime='scroll']");
+scrollAnima.init();
